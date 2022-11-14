8b21bc0436e831f14ea95d1324d1d7dbd14002db
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-14 23:02:07 +0600

    change: different routes depending on login

--- src/common/pages/Pages.tsx
+++ src/common/pages/Pages.tsx
@@ -8,6 +8,7 @@ import { Login } from 'features/auth/login/Login'
 import { PasswordRecovery } from 'features/auth/password-recovery/PasswordRecovery'
 import { Profile } from 'features/auth/profile/Profile'
 import { Registration } from 'features/auth/registration/Registration'
+import { useAppSelector } from 'utils/hooks'
 
 export const PATH = {
   LOGIN: '/login',
@@ -18,15 +19,27 @@ export const PATH = {
 }
 
 export const Pages = () => {
+  const authMe = useAppSelector(state => state.auth.authMe)
+
   return (
     <div>
       <Routes>
-        <Route path={'/'} element={<Navigate to={PATH.LOGIN} />} />
-        <Route path={PATH.LOGIN} element={<Login />} />
+        {authMe ? (
+          <>
+            <Route path={'/'} element={<Navigate to={PATH.PROFILE} />} />
+            <Route path={PATH.LOGIN} element={<Navigate to={PATH.PROFILE} />} />
+            <Route path={PATH.PROFILE} element={<Profile />} />
+          </>
+        ) : (
+          <>
+            <Route path={'/'} element={<Navigate to={PATH.LOGIN} />} />
+            <Route path={PATH.PROFILE} element={<Navigate to={PATH.LOGIN} />} />
+            <Route path={PATH.LOGIN} element={<Login />} />
+          </>
+        )}
         <Route path={PATH.REGISTRATION} element={<Registration />} />
         <Route path={PATH.PASSWORD_RECOVERY} element={<PasswordRecovery />} />
         <Route path={PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
-        <Route path={PATH.PROFILE} element={<Profile />} />
         <Route path={'/404'} element={<PageNotFound />} />
         <Route path={'*'} element={<Navigate to={'/404'} />} />
       </Routes>
