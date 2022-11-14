ae961e107cc6a026df7a1535b13a778add90ae62
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-14 23:21:42 +0600

    feat: me request

--- src/features/auth/auth-API.ts
+++ src/features/auth/auth-API.ts
@@ -1,8 +1,20 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'https://neko-back.herokuapp.com/2.0',
+  baseURL: 'http://localhost:7542/2.0',
   withCredentials: true,
 })
 
-export const authAPI = {}
+export const authAPI = {
+  me() {
+    return instance.post<AuthMeType>('/auth/me', {})
+  },
+}
+
+// types
+type AuthMeType = {
+  created: string
+  email: string
+  name: string
+  publicCardPacksCount: number
+}
