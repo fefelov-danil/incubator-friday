279f2fe0c428e5732a39d2fc2cc86b0bf28e940a
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-15 19:57:21 +0600

    new: EditableSpan

---
+++ src/common/editableSpan/EditableSpan.tsx
@@ -0,0 +1,82 @@
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+
+import CheckIcon from '@mui/icons-material/Check'
+import EditIcon from '@mui/icons-material/Edit'
+import IconButton from '@mui/material/IconButton'
+
+import s from './EditableSpan.module.css'
+
+type EditAbleSpanPropsType = {
+  value: string
+  updateTitle: (newTitle: string) => void
+  disabled: boolean
+}
+
+export const EditableSpan: React.FC<EditAbleSpanPropsType> = ({ value, updateTitle, disabled }) => {
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [title, setTitle] = useState<string>(value)
+  const [error, setError] = useState<boolean | string>(false)
+
+  const onEditMode = () => {
+    !disabled && setEditMode(true)
+  }
+  const offEditMode = () => {
+    if (title === value) {
+      setEditMode(false)
+
+      return
+    }
+    const itemTitle = title.trim()
+
+    if (itemTitle) {
+      updateTitle(title)
+      setEditMode(false)
+    } else {
+      setError('The field is required')
+    }
+  }
+
+  const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+    const itemTitle = e.currentTarget.value.trim()
+
+    if (error && itemTitle) setError(false)
+    if (!itemTitle) setError('The field is required')
+    if (itemTitle.length >= 100) setError('Length no more than 100 characters')
+  }
+  const onKeyDownChangeText = (e: KeyboardEvent<HTMLInputElement>) => {
+    e.key === 'Enter' && offEditMode()
+  }
+
+  return (
+    <span className={s.editableSpan}>
+      {editMode ? (
+        <span className={s.editMode}>
+          <input
+            className={error ? s.errorInp : ''}
+            value={title}
+            autoFocus
+            onBlur={offEditMode}
+            onChange={onChangeSetTitle}
+            onKeyDown={onKeyDownChangeText}
+          />
+          {editMode && error && <span className={s.formError}>{error}</span>}
+          <IconButton
+            className={s.changeTextIcon}
+            onClick={offEditMode}
+            disabled={title ? disabled : true}
+            sx={{ color: '#333', padding: '0px' }}
+            size={'small'}
+          >
+            <CheckIcon className={!title ? s.disabledIcon : ''} />
+          </IconButton>
+        </span>
+      ) : (
+        <span onClick={onEditMode} className={s.textMode}>
+          {value}
+          <EditIcon className={s.editTextIcon} sx={{ fontSize: 15, color: '#777' }} />
+        </span>
+      )}
+    </span>
+  )
+}
