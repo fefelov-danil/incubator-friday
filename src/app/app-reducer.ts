368728b1e0295da89f05234cede3cbff2ed19105
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-16 15:58:58 +0600

    feat: app loading in app-reducer.ts

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,12 +1,15 @@
-const appInitialState: AppStateType = {
-  appError: null,
-  appStatus: 'idle',
+const appInitialState = {
+  appError: null as null | string,
+  appLoading: true,
+  appStatus: 'idle' as AppStatusType,
 }
 
 export const appReducer = (state: AppStateType = appInitialState, action: AppActionsType) => {
   switch (action.type) {
     case 'APP/SET-APP-ERROR':
       return { ...state, appError: action.error }
+    case 'APP/SET-APP-LOADING':
+      return { ...state, appLoading: action.appLoading }
     case 'APP/SET-APP-STATUS':
       return { ...state, appStatus: action.appStatus }
     default:
@@ -23,6 +26,13 @@ export const setAppErrorAC = (error: string | null) => {
   } as const
 }
 
+export const setAppLoading = (appLoading: boolean) => {
+  return {
+    type: 'APP/SET-APP-LOADING',
+    appLoading,
+  } as const
+}
+
 export const setAppStatusAC = (appStatus: AppStatusType) => {
   return {
     type: 'APP/SET-APP-STATUS',
@@ -31,9 +41,10 @@ export const setAppStatusAC = (appStatus: AppStatusType) => {
 }
 
 // types
-type AppStateType = {
-  appError: string | null
-  appStatus: AppStatusType
-}
-export type AppActionsType = ReturnType<typeof setAppErrorAC | typeof setAppStatusAC>
+type AppStateType = typeof appInitialState
+
+export type AppActionsType =
+  | ReturnType<typeof setAppErrorAC>
+  | ReturnType<typeof setAppStatusAC>
+  | ReturnType<typeof setAppLoading>
 export type AppStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
