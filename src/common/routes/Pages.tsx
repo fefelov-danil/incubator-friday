fac7d14f305a1caf8e398667b8d26c19faa27d42
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-21 02:02:18 +0600

    change: routes

--- src/common/routes/Pages.tsx
+++ src/common/routes/Pages.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 
 import { PageNotFound } from 'common/404/PageNotFound'
+import { PrivateRoutes } from 'common/routes/PrivateRoutes'
 import { ChangePassword } from 'features/auth/change-password/ChangePassword'
 import { Login } from 'features/auth/login/Login'
 import { PasswordRecovery } from 'features/auth/password-recovery/PasswordRecovery'
 import { Profile } from 'features/auth/profile/Profile'
 import { Registration } from 'features/auth/registration/Registration'
+import { Packs } from 'features/packs/Packs'
 import { useAppSelector } from 'utils/hooks'
 
 export const PATH = {
@@ -16,10 +18,29 @@ export const PATH = {
   PASSWORD_RECOVERY: '/password-recovery',
   CHANGE_PASSWORD: '/set-new-password/:token',
   PROFILE: '/profile',
+  PACKS: '/packs',
 }
 
 export const Pages = () => {
   const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+
+  // return (
+  //   <>
+  //     <Routes>
+  //       <Route element={<PrivateRoutes />}>
+  //         <Route path={'/'} element={<Navigate to={PATH.PROFILE} />} />
+  //         {/*<Route path={'/login'} element={<Profile />} />*/}
+  //         <Route path={PATH.PROFILE} element={<Profile />} />
+  //       </Route>
+  //       <Route path={PATH.LOGIN} element={<Login />} />
+  //       <Route path={PATH.REGISTRATION} element={<Registration />} />
+  //       <Route path={PATH.PASSWORD_RECOVERY} element={<PasswordRecovery />} />
+  //       <Route path={PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
+  //       <Route path={'/404'} element={<PageNotFound />} />
+  //       <Route path={'*'} element={<Navigate to={'/404'} />} />
+  //     </Routes>
+  //   </>
+  // )
 
   return (
     <div>
@@ -29,17 +50,18 @@ export const Pages = () => {
             <Route path={'/'} element={<Navigate to={PATH.PROFILE} />} />
             <Route path={PATH.LOGIN} element={<Navigate to={PATH.PROFILE} />} />
             <Route path={PATH.PROFILE} element={<Profile />} />
+            <Route path={PATH.PACKS} element={<Packs />} />
           </>
         ) : (
           <>
             <Route path={'/'} element={<Navigate to={PATH.LOGIN} />} />
             <Route path={PATH.PROFILE} element={<Navigate to={PATH.LOGIN} />} />
             <Route path={PATH.LOGIN} element={<Login />} />
+            <Route path={PATH.REGISTRATION} element={<Registration />} />
+            <Route path={PATH.PASSWORD_RECOVERY} element={<PasswordRecovery />} />
+            <Route path={PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
           </>
         )}
-        <Route path={PATH.REGISTRATION} element={<Registration />} />
-        <Route path={PATH.PASSWORD_RECOVERY} element={<PasswordRecovery />} />
-        <Route path={PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
         <Route path={'/404'} element={<PageNotFound />} />
         <Route path={'*'} element={<Navigate to={'/404'} />} />
       </Routes>
