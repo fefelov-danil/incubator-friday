876a353cb07926d5876f636e5e17b8f101d12623
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-21 02:06:55 +0600

    feat: filters off btn

---
+++ src/features/packs/filters/filtersOff/FiltersOff.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff'
+
+import s from './FiltersOff.module.css'
+
+type FiltersOffPropsType = {
+  turnOffFilters: () => void
+}
+
+export const FiltersOff: React.FC<FiltersOffPropsType> = ({ turnOffFilters }) => {
+  return (
+    <button className={s.filtersOff} onClick={turnOffFilters}>
+      <FilterAltOffIcon />
+    </button>
+  )
+}
