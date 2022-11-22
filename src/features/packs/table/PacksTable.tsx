8f299d02813b3502a434acefd8a00c8f8f08bc6c
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-22 16:24:30 +0600

    refactor: naming

--- src/features/packs/table/PacksTable.tsx
+++ src/features/packs/table/PacksTable.tsx
@@ -4,15 +4,15 @@ import Paper from '@mui/material/Paper'
 import Table from '@mui/material/Table'
 import TableContainer from '@mui/material/TableContainer'
 
-import { Tbody } from 'features/packs/table/body/Tbody'
-import { Thead } from 'features/packs/table/head/Thead'
+import { TbodyPacks } from 'features/packs/table/body/TbodyPacks'
+import { TheadPacks } from 'features/packs/table/head/TheadPacks'
 
 export const PacksTable = () => {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <Thead />
-        <Tbody />
+      <Table>
+        <TheadPacks />
+        <TbodyPacks />
       </Table>
     </TableContainer>
   )
