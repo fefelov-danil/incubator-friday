67011606f82872a34e02b70ccac2c0825f015c95
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-23 22:50:36 +0600

    refactor: change callback location

--- src/features/packs/filters/Filters.tsx
+++ src/features/packs/filters/Filters.tsx
@@ -8,14 +8,6 @@ import { InputSearch } from 'features/packs/filters/inputSearch/InputSearch'
 import { MinMaxCards } from 'features/packs/filters/slider/MinMaxCards'
 
 export const Filters = () => {
-  const changeSearchText = (value: string) => {
-    console.log(value)
-  }
-
-  const changeSlider = (currentMinMax: number[]) => {
-    console.log(currentMinMax)
-  }
-
   const changeAllOrMyPacks = (my: boolean) => {
     console.log(my)
   }
@@ -28,7 +20,7 @@ export const Filters = () => {
     <div className={s.filters}>
       <div className={s.search}>
         <p className={s.filterName}>Search</p>
-        <InputSearch placeholder={'Provide your text'} onChangeText={changeSearchText} />
+        <InputSearch placeholder={'Provide your text'} />
       </div>
       <div className={s.allOrMyPacks}>
         <p className={s.filterName}>Show packs cards</p>
@@ -36,7 +28,7 @@ export const Filters = () => {
       </div>
       <div className={s.slider}>
         <p className={s.filterName}>Number of cards</p>
-        <MinMaxCards initialMinMax={[0, 50]} changeSlider={changeSlider} />
+        <MinMaxCards />
       </div>
       <div className={s.filtersOff}>
         <FiltersOff turnOffFilters={turnOffFilters} />
