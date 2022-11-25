255664318c36ebfb09282578df78831bfc6b369c
splinshady <splin.shady@gmail.com>
2022-11-25 15:40:51 +0300

    feat: add search message

--- src/features/packs/filters/inputSearch/InputSearch.tsx
+++ src/features/packs/filters/inputSearch/InputSearch.tsx
@@ -28,8 +28,12 @@ export const InputSearch: React.FC<InputSearchPropsType> = ({ whose, ...restProp
   const dispatch = useAppDispatch()
   const filterSearchValuePacks = useAppSelector(state => state.packs.filterSearchValue)
   const filterSearchValueCards = useAppSelector(state => state.cards.filterSearchValue)
+  const contentPacks = useAppSelector(state => state.packs.cardPacks)
+  const contentCards = useAppSelector(state => state.cards.cards)
 
   let filterSearchValue = whose === 'packs' ? filterSearchValuePacks : filterSearchValueCards
+  let content = whose === 'packs' ? contentPacks : contentCards
+  let contentLength = content ? content.length : 0
 
   const [value, setValue] = useState<string>(filterSearchValue)
   const debouncedValue = useDebounce<string>(value, 700)
@@ -60,6 +64,11 @@ export const InputSearch: React.FC<InputSearchPropsType> = ({ whose, ...restProp
         className={s.inputSearch}
         {...restProps}
       />
+      {value.length > 0 && contentLength === 0 && (
+        <span
+          className={s.error_message}
+        >{`"Колоды с введенным название не найдены. Измените параметры запроса" или что то типо этого`}</span>
+      )}
     </p>
   )
 }
