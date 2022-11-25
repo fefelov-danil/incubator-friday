4452c69de71989ed9506378595daf82609891a44
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-25 16:25:37 +0600

    cards: actions

--- src/features/cards/Cards.tsx
+++ src/features/cards/Cards.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
+import SchoolIcon from '@mui/icons-material/School'
+import IconButton from '@mui/material/IconButton/IconButton'
 import { NavLink } from 'react-router-dom'
 
 import { useAppDispatch, useAppSelector } from '../../utils/hooks'
@@ -13,23 +18,56 @@ import { Paginator } from 'common/paginator/Paginator'
 import { PATH } from 'common/routes/Pages'
 import { CardsTable } from 'features/cards/table/CardsTable'
 import { InputSearch } from 'features/packs/filters/inputSearch/InputSearch'
-import { setCurrentPacksPageAC, setPagePacksCountAC } from 'features/packs/packs-reducer'
+import {
+  deletePackTC,
+  setCurrentPacksPageAC,
+  setPagePacksCountAC,
+  updatePackTC,
+} from 'features/packs/packs-reducer'
 
 export const Cards = () => {
   const dispatch = useAppDispatch()
   const myId = useAppSelector(state => state.auth.profile._id)
   const cardPacks = useAppSelector(state => state.packs.cardPacks)
+  const packId = useAppSelector(state => state.cards.currentPackId)
   const userPackId = useAppSelector(state => state.cards.packUserId)
   const cardsPack_id = useAppSelector(state => state.cards.currentPackId)
-  const cards = useAppSelector(state => state.cards)
   const page = useAppSelector(state => state.cards.page)
   const pageCount = useAppSelector(state => state.cards.pageCount)
   const cardsTotalCount = useAppSelector(state => state.cards.cardsTotalCount)
 
-  console.log(cards)
+  const [openActions, setOpenActions] = useState(false)
+
+  const actions = useRef(null as HTMLDivElement | null)
+
+  useEffect(() => {
+    if (!openActions) return
+
+    const handleClick = (e: any) => {
+      if (!actions.current) return
+      if (!actions.current.contains(e.target)) {
+        setOpenActions(false)
+      }
+    }
+
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  }, [openActions])
 
   const addNewCard = () => {
     dispatch(createNewCardTC({ cardsPack_id }))
+  }
+  const learnToPack = () => {
+    console.log('learnToPack')
+  }
+  const editPack = () => {
+    dispatch(updatePackTC({ _id: packId, name: 'edited PACK' }))
+  }
+  const deletePack = () => {
+    dispatch(deletePackTC(packId))
   }
 
   const renderMainActions = (myId: string, userId: string) => {
@@ -43,7 +81,31 @@ export const Cards = () => {
     if (myId === userId) {
       return (
         <div className={s.titleAndBtn}>
-          <h1>{packName}</h1>
+          <div>
+            <h1>{packName}</h1>
+            <div ref={actions} className={s.actions}>
+              <IconButton onClick={() => setOpenActions(!openActions)} className={s.dotsForActions}>
+                <MoreVertIcon />
+              </IconButton>
+              <div
+                className={
+                  openActions
+                    ? `${s.actionsPopUp} ${s.actionsIsOpened}`
+                    : `${s.actionsPopUp} ${s.actionsIsClosed}`
+                }
+              >
+                <button className={s.action} onClick={editPack}>
+                  <EditIcon sx={{ fontSize: 19 }} /> Edit
+                </button>
+                <button className={s.action} onClick={deletePack}>
+                  <DeleteIcon sx={{ fontSize: 19 }} /> Delete
+                </button>
+                <button className={s.action} onClick={learnToPack}>
+                  <SchoolIcon sx={{ fontSize: 19 }} /> Learn
+                </button>
+              </div>
+            </div>
+          </div>
           <Button onClick={addNewCard}>Add New Card</Button>
         </div>
       )
@@ -52,7 +114,7 @@ export const Cards = () => {
       return (
         <div className={s.titleAndBtn}>
           <h1>{packName}</h1>
-          <Button onClick={addNewCard}>Add New Card</Button>
+          <Button onClick={learnToPack}>Lern to pack</Button>
         </div>
       )
     }
