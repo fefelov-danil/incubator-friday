b4eb3bd03184fe3f40583b6b858c2ed5f6ecde07
Daniil Fefelov <fefelov.danil@gmail.com>
2022-11-28 18:50:53 +0600

    fix: FiltersOff.tsx

--- src/features/packs/filters/filtersOff/FiltersOff.tsx
+++ src/features/packs/filters/filtersOff/FiltersOff.tsx
@@ -19,14 +19,17 @@ export const FiltersOff = () => {
   const dispatch = useAppDispatch()
   const maxCardsCount = useAppSelector(state => state.packs.maxCardsCount)
   const appStatus = useAppSelector(state => state.app.appStatus)
+  const sortByAllMy = useAppSelector(state => state.packs.sortByAllMy)
 
   const turnOffFilters = () => {
+    if (sortByAllMy === 'my') {
+      dispatch(setRerenderAC(false))
+    }
     dispatch(setCurrentPacksPageAC(1))
     dispatch(setSortByAllMyAC('all'))
     dispatch(setSortMinMaxCardsAC(0, maxCardsCount))
     dispatch(setFilterToPacksFromInputSearchAC(''))
     dispatch(setFilterToCardsFromInputSearchAC(''))
-    dispatch(setRerenderAC(false))
   }
 
   return (
