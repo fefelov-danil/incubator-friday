20fb7662a5e918d1e3c93662c827ab2db967ce7c
Daniil Fefelov <fefelov.danil@gmail.com>
2022-12-06 15:52:10 +0600

    refactor: DeleteModal removed unnecessary

--- src/features/cards/modals/DeleteModal.tsx
+++ src/features/cards/modals/DeleteModal.tsx
@@ -10,29 +10,23 @@ import { useAppDispatch, useAppSelector } from 'utils/hooks'
 
 export const DeleteModal = () => {
   const dispatch = useAppDispatch()
-  const packId = useAppSelector(state => state.cards.currentPackId)
   const cardsPack_id = useAppSelector(state => state.cards.currentPackId)
   const cardPacks = useAppSelector(state => state.packs.cardPacks)
   const appStatus = useAppSelector(state => state.app.appStatus)
 
   const [openModal, setOpenModal] = useState<boolean | null>(null)
-  const [inputValue, setInputValue] = useState<string | undefined>('')
-  const [isChecked, setIsChecked] = useState<boolean | undefined>(false)
 
   const deletePack = () => {
     dispatch(deletePackTC(cardsPack_id, 'cards'))
     setOpenModal(false)
   }
 
-  const getPackParam = (packId: string) => {
-    if (cardPacks !== null && cardPacks.length > 0) {
-      const pack = cardPacks.find(p => p._id === packId)
+  let packName
 
-      if (pack) {
-        setInputValue(pack.name)
-        setIsChecked(pack.private)
-      }
-    }
+  if (cardPacks) {
+    const pack = cardPacks.find(pack => pack._id === cardsPack_id)
+
+    packName = pack?.name
   }
 
   return (
@@ -40,18 +34,14 @@ export const DeleteModal = () => {
       title={'Delete pack'}
       setOpenModal={setOpenModal}
       childrenOpenModal={
-        <button
-          className={s.action}
-          onClick={() => getPackParam(packId)}
-          disabled={appStatus === 'loading'}
-        >
+        <button className={s.action} disabled={appStatus === 'loading'}>
           <DeleteIcon sx={{ fontSize: 19 }} /> Delete
         </button>
       }
       openFromProps={openModal}
     >
       <p>
-        Do you really want to remove <b>{inputValue}</b>?
+        Do you really want to remove <b>{packName}</b>?
       </p>
       <div className={'modalButtonBlock'}>
         <Button className={'close'} onClick={() => setOpenModal(false)}>
